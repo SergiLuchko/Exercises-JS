8b6d84bc41e5cfb72568a10f0c8198e0cfd0083b
SergiLuchko <riziuswow@gmail.com>
2024-09-17 21:10:55 +0200

    ejemplos de js,html y css

---
+++ ejemplo.js
@@ -0,0 +1,34 @@
+//const nom = "sergi";
+//const cognom = "LUCHKO";
+//let correccionNombre = nom.charAt(0).toUpperCase() + nom.slice(1).toLowerCase();
+//let correccionApellido = cognom.charAt(0).toUpperCase() + cognom.slice(1).toLocaleLowerCase();
+//alert(correccionNombre);
+//alert(correccionApellido);
+
+/* let repetida = "Pau tiene dos manos y yo dos pies"
+let posicion = repetida.indexOf("dos");
+let posicion2 = repetida.lastIndexOf("dos");
+alert(posicion);
+alert(posicion2); */
+
+
+
+/* let frase = "Yo tengo tres ojeras y tres manos";
+let valor = "tres";
+let posicion = frase.indexOf(valor);
+let posicion2 = frase.lastIndexOf(valor);
+console.log(posicion,posicion2);
+let suma = posicion2 + valor.length;
+console.log(suma); */
+
+
+
+/* let frase = "coche se va a romper";
+let word = "romper";
+let resultado = frase.endsWith(word);
+console.log(resultado); */
+
+
+
+
+
